ef7bdcd525351b2560191e9225decee375101c4e
Brandon Huffman <bt_huffman@msn.com>
2019-01-23 11:19:59 -0700

    In accordance with project documentation specs, created the file directory structure, the milesone-1.php file including all the information required to describe the purpose and use of my personal website project.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Website Project</title>
+	</head>
+	<body>
+		<h1>Professional Website</h1>
+		<h2>1. Purpose:</h2>
+		<p>To create a professional website that highlights my skills, knowledge and experience in a dynamic and visually
+			appealing format. </p>
+		<h2>2. Audience:</h2>
+		<p>My target audience is potential employers and customers.</p>
+		<h2>3. Goal:</h2>
+		<p>To gain employment with a progressive company or to attract clientele.</p>
+		<h1>Persona</h1>
+		<p>Maya Peterson, Human Resources Manager at Urban Sourcing. Maya is 35 years old with 10 years of experience
+			looking for front-end and back-end web developers with experience in the latest web languages including HTML 5,
+			CSS 3, JavaScript ES6, MySql 5.7, PHP 7.2, Bootstrap 4, and Angular 6. She is enthusiastic and driven to hire
+			the best talent. Having hired successful Deep Dive Coding alumni in the past she is always excited to speak to
+			talented graduates from the program. To verify candidates skills she often looks at their personal projects to
+			assess their strengths and weaknesses. This is her primary reason for visiting my site today.</p>
+		<p>Due to the front-end nature of my personal web project she will primarily be looking at the execution of the
+			corresponding languages including HTML 5, CSS 3, Bootstrap 4, jQuery, Javascript, UX & UI Design, and built in
+			securities for the site. After a thorough examination of the technical prowess, style, and quality of the site,
+			Maya will have a greater understanding of my capabilities as a web developer and my suitability for various
+			positions at Urban Sourcing. She gets frustrated when the website has a slow upload, a design that isn't
+			particularly user friendly, lacks visual appeal, isn't easy to read or demonstrates inconsistencies between
+			coding styles and frameworks. If I can pass through all these filters unscathed she'll likely want to
+			check out the other projects on my github to help her make a final evaluation. </p>
+	</body>
+</html>
