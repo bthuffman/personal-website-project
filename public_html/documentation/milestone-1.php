79355c59ce6ae8734bf933765026aaeb9f84af8d
Brandon Huffman <bt_huffman@msn.com>
2019-01-29 20:51:04 -0700

    Added details to the Audience, Goal and technology device info to the Persona.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,11 +10,13 @@
 		<p>To create a professional website that highlights my skills, knowledge and experience in a dynamic and visually
 			appealing format. </p>
 		<h2>2. Audience:</h2>
-		<p>My target audience is potential employers and customers.</p>
+		<p>The site is targeting professional individuals, specifically potential employers and/or customers nationwide who are looking to hire a developer for web based services and technology. The audience includes though is not limited to hiring managers, human resource departments, peers, recruiters, and others in need of web based solutions and services.</p>
 		<h2>3. Goal:</h2>
-		<p>To gain employment with a progressive company or to attract clientele.</p>
+		<p>To gain employment at a local business that can put my new web development skills to good use. Ideally such a business would be a software specific firm that invests in their employees with fair pay, good benefits, and a decent amount of time off. One that promotes involvement in the local tech community would also be a big plus.</p>
 		<h2>4. Persona:</h2>
-		<p>Maya Peterson, Human Resources Manager at Urban Sourcing. Maya is 35 years old with 10 years of experience
+		<p>Maya Peterson, Human Resources Manager at Urban Sourcing uses a I-phone 8 Plus and a Intel Core i7-8550U laptop
+			with 12 GB of ram
+			running windows on a 64-bit Operating System. Maya is 35 years old with 10 years of experience
 			looking for front-end and back-end web developers with experience in the latest web languages including HTML 5,
 			CSS 3, JavaScript ES6, MySql 5.7, PHP 7.2, Bootstrap 4, and Angular 6. She is enthusiastic and driven to hire
 			the best talent. Having hired successful Deep Dive Coding alumni in the past she is always excited to speak to
@@ -24,7 +26,7 @@
 			corresponding languages including HTML 5, CSS 3, Bootstrap 4, jQuery, Javascript, UX & UI Design, and built in
 			securities for the site. After a thorough examination of the technical prowess, style, and quality of the site,
 			Maya will have a greater understanding of my capabilities as a web developer and my suitability for various
-			positions at Urban Sourcing. She gets frustrated when the website has a slow upload, a design that isn't
+			positions at Urban Sourcing. he gets fruSstrated when the website has a slow upload, a design that isn't
 			particularly user friendly, lacks visual appeal, isn't easy to read or demonstrates inconsistencies between
 			coding styles and frameworks. If I can pass through all these filters unscathed she'll likely want to
 			check out the other projects on my github to help her make a final evaluation. </p>
