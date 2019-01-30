2eecf52df511846c7368d353cb6984a257c28738
Brandon Huffman <bt_huffman@msn.com>
2019-01-30 16:05:34 -0700

    Added Persona picture and a complete user story.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -14,8 +14,9 @@
 		<h2>3. Goal:</h2>
 		<p>To gain employment at a local business that can put my new web development skills to good use. Ideally such a business would be a software specific firm that invests in their employees with fair pay, good benefits, and a decent amount of time off. One that promotes involvement in the local tech community would also be a big plus.</p>
 		<h2>4. Persona:</h2>
-		<p>Maya Peterson, Human Resources Manager at Urban Sourcing uses a I-phone 8 Plus and a Intel Core i7-8550U laptop
-			with 12 GB of ram
+		<img src="maya-peterson.jpeg">
+		<p>Maya Peterson, Human Resources Manager at Urban Sourcing uses the chrome browser on both an I-phone 8 Plus and
+			an Intel Core i7-8550U laptop with 12 GB of ram
 			running windows on a 64-bit Operating System. Maya is 35 years old with 10 years of experience
 			looking for front-end and back-end web developers with experience in the latest web languages including HTML 5,
 			CSS 3, JavaScript ES6, MySql 5.7, PHP 7.2, Bootstrap 4, and Angular 6. She is enthusiastic and driven to hire
@@ -30,5 +31,34 @@
 			particularly user friendly, lacks visual appeal, isn't easy to read or demonstrates inconsistencies between
 			coding styles and frameworks. If I can pass through all these filters unscathed she'll likely want to
 			check out the other projects on my github to help her make a final evaluation. </p>
+		<h1>User Story:</h1>
+		<p> Maya needs to sample one of my web-based projects to determine how strong my front-end web development skills are.</p>
+		<h2>Use Case:</h2>
+		<h3>Title:</h3>
+		<p> Maya Visits My Project.</p>
+		<h3>Description:</h3>
+		<p> Maya selects my Personal Web Project to judge the quality of my work and to see how I may or may not be a fit for Urban Sourcing. </p>
+		<h3>Name of user and their role:</h3>
+		<p>Maya: human resource manager looking for top talent to acquire for her web-based software company. </p>
+		<h3>Usage preconditions:</h3>
+		<p>Must have a computer or cellphone and internet access.</p>
+		<h3>User post-conditions:</h3>
+		<p>Have an understanding of my front-end development abilities.</p>
+		<h3>Interaction flow:</h3>
+		<ul>
+			<li>Maya visits my cnm boot camp projects at <a href="https://bootcamp-coders.cnm.edu/~bhuffman1/">https://bootcamp-coders.cnm.edu/~bhuffman1/</a></li>
+			<li>Server returns the bootcamp-coders server's index "of/~bhuffman1" page.</li>
+			<li>Maya navigates to my "personal-website-project" folder and clicks it open.</li>
+			<li>Server returns the page containing the projects contents (index "of/~bhuffman1/personal-website-project".</li>
+			<li>Maya clicks on the public_html folder.</li>
+			<li>Server returns the contents of the public_html folder.</li>
+			<li>Maya clicks on the documentation folder.</li>
+			<li>Server returns the contents of the documentation folder.</li>
+			<li>Maya clicks on the index.html to open up the landing page of my professional-web-project.</li>
+			<li>Server returns the landing page of my professional-web-project.</li>
+			<li>Maya looks around the site and plays around with it's functionality to determine it's quality and completeness.</li>
+		</ul>
+		<h3>Frequency of use:</h3>
+		<p>Maybe one or two times a day.</p>
 	</body>
 </html>
