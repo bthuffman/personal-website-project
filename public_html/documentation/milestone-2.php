665a65979053e88b766d64f60e8ed3795d741ecb
Brandon Huffman <bt_huffman@msn.com>
2019-02-04 14:24:25 -0700

    Added my wireframes

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>Personal Website Project: Milestone 2a</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>My Professional Website Project will consist of one page with the following sections:</p>
+		<p><Strong>Navigation:</Strong> This bar will include my name and a nav selection to the other sections which
+		collapses into a hamburger button when the viewport shrinks. I would like this navigation bar to be fixed and
+		follow the user as they scroll down the page. </p>
+		<p><strong>Introduction:</strong>  This section will contain some stylized text including a short welcome, my name,
+			career goals (web developer | web designer), a link to my contact me section (see description below) and links
+		to my github and linked in. </p>
+		<p><strong>About Me:</strong> This will contain a short heading like "Create. Augment. Repeat." followed by a
+		paragraph briefly showcasing my strengths, personality, and aspirations. </p>
+		<p><strong>Skills:</strong> an overview of a few of my top skills each with a heading followed by short description detailing each.
+		One of these skills will be coding languages.</p>
+		<p><strong>Experience:</strong> This section will detail the most pertinent job and educational experiences that
+		expand my abilities into the field of software development. This is meant to highlight only a few so I might attach
+		a link to my full resume here. I would like to have the logo of the companies I worked for here which when clicked
+		opens up a new tab with their homepage. </p>
+		<p><strong>Contact Me:</strong>This section will contain a standard and secure contact form followed by the footer
+			which will contain my email address, additional links to my github and linkedIn. </p>
+		<h1>Wireframes</h1>
+		<p>Desktop Wireframe</p>
+		<img src="desktop-wireframe.png" alt="desktop version of wireframe"/>
+		<p>Mobile Wireframe</p>
+		<img src="mobile-wireframe.png" alt="mobile version of wireframe"/>
+
+	</body>
+</html>
