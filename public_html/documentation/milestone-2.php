256fadbbb88e17bba842af938815e6affa0472e6
Brandon Huffman <bt_huffman@msn.com>
2019-02-04 18:58:49 -0700

    Added additional details to various sections including the wireframe section and since it is a mobile first website the mobile wireframes were moved on top of the desktop one.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,22 +12,28 @@
 		follow the user as they scroll down the page. </p>
 		<p><strong>Introduction:</strong>  This section will contain some stylized text including a short welcome, my name,
 			career goals (web developer | web designer), a link to my contact me section (see description below) and links
-		to my github and linked in. </p>
+		to my github and linked in. There will also be a large background image of Lake Tahoe or one of my other favorite
+		US lakes.</p>
 		<p><strong>About Me:</strong> This will contain a short heading like "Create. Augment. Repeat." followed by a
 		paragraph briefly showcasing my strengths, personality, and aspirations. </p>
-		<p><strong>Skills:</strong> an overview of a few of my top skills each with a heading followed by short description detailing each.
-		One of these skills will be coding languages.</p>
+		<p><strong>Skills:</strong> an overview of a few of my top skills that highlight my ability to achieve in the software
+			development industry. Each skill will have a heading followed by short description detailing them.
+		One of these skills will be coding languages and I will rate my proficiencies with various languages in a format that is
+			easily readable at a glance.</p>
 		<p><strong>Experience:</strong> This section will detail the most pertinent job and educational experiences that
 		expand my abilities into the field of software development. This is meant to highlight only a few so I might attach
-		a link toa pdf of my resume here. I would like to have the logo of the companies I worked for here which when clicked
-		opens up a new tab with their homepage. </p>
+		a link to a pdf of my resume here. I would like to have the logo of the companies I worked for here which when clicked
+		opens up a new tab with their homepage. I am also considering making this field more like a cover flow. This means
+			that hte user could click left and right arrows in order to scroll through the covers each of which would
+			detail one of my many previous jobs and educational experiences. </p>
 		<p><strong>Contact Me:</strong>This section will contain a standard and secure contact form followed by the footer
-			which will contain my email address, additional links to my github and linkedIn. </p>
+			which will contain a link to my email address, github and linkedIn. </p>
 		<h2>Wireframes</h2>
-		<h3>Desktop Wireframe</h3>
-		<img src="desktop-wireframe.png" alt="desktop version of wireframe"/>
+		<p>This site will be build following a mobile first strategy and will be fully responsive on both small and
+		large screens. The following wireframes were created on <a href="http://balsamiq.com">https:balsamiq.com</a>.</p>
 		<h3>Mobile Wireframe</h3>
 		<img src="mobile-wireframe.png" alt="mobile version of wireframe"/>
-
+		<h3>Desktop Wireframe</h3>
+		<img src="desktop-wireframe.png" alt="desktop version of wireframe"/>
 	</body>
 </html>
